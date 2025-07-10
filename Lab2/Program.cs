1e38bdd9f2a518736dfa8e5a8d08565bf795f973
Rousan21 <adamrousan21@gmail.com>
2025-07-10 05:53:11 +0000

    Added Lab2 code

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,9 +1,97 @@
-﻿namespace Lab2;
+﻿using System;
 
-class Program
+/*
+Name: Adam Al-Rousan
+Lab: IT-1050 - Lab 2
+*/
+
+namespace Lab2
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            // Part 1: Operator Practice
+            int a = 10;
+            int b = 5;
+            int c = 15;
+
+            Console.WriteLine("a > b: " + (a > b));
+            Console.WriteLine("a < c: " + (a < c));
+            Console.WriteLine("a > b && a > c: " + (a > b && a > c));
+            Console.WriteLine("a > b || a > c: " + (a > b || a > c));
+
+            Console.WriteLine(); // spacing
+
+            // Part 2: Boolean Logic
+            bool isRaining = true;
+            bool haveUmbrella = false;
+
+            if (isRaining && !haveUmbrella)
+            {
+                Console.WriteLine("Take an umbrella!");
+            }
+            else
+            {
+                Console.WriteLine("You're good to go!");
+            }
+
+            Console.WriteLine();
+
+            // Part 3: Movie Ticket Pricing
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 5)
+            {
+                Console.WriteLine("Ticket is free!");
+            }
+            else if (age >= 5 && age <= 12)
+            {
+                Console.WriteLine("Child ticket: $5");
+            }
+            else if (age >= 13 && age <= 64)
+            {
+                Console.WriteLine("Standard ticket: $10");
+            }
+            else
+            {
+                Console.WriteLine("Senior ticket: $6");
+            }
+
+            Console.WriteLine();
+
+            // Part 4: Switch Statement for Days
+            Console.Write("Enter a number (1-7): ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day!");
+                    break;
+            }
+        }
     }
 }
